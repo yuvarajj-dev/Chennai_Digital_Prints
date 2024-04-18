135efd03326a8326c1c08ab47e3fafb1314173d1
yuvarajj-dev <yuvarajj.developer@gmail.com>
2024-04-18 13:17:13 +0530

    scrolltrigger update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import Footer from './Components/Footer/Footer';
 import Common_product from './Components/Product/Common_product';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(_ScrollTrigger)
 function App() {
+    window.addEventListener("load",()=>{
+      ScrollTrigger.refresh();
+    })
+ 
   return (
     <div id="app" className="App">
       <NAVBAR />

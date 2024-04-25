98eca837b002f89ad6f53b1cbeff447c7cbe2ef3
yuvarajj-dev <yuvarajj.developer@gmail.com>
2024-04-25 12:54:33 +0530

    Get

--- src/Components/Product/Card/Carddata.jsx
+++ src/Components/Product/Card/Carddata.jsx
@@ -1,22 +1,20 @@
 const cardlist = [
   {
     img_url: "https://www.printlipi.com/images/standard-visitingcard.png",
-    name: "Visiting Cards",
-    heading: "Visiting Cards",
-    typestitle: "Similer Products",
-    types: [
-      "Standard Visiting Card",
-      "Premium Visiting Card",
-      "Matte Visiting Card",
-      "Glossy Visiting Card",
-      "Embossed Visiting Card",
-      "Spot UV Visiting Card",
-      "Folded Visiting Card",
-      "Die-cut Visiting Card",
-      "Metallic Visiting Card",
-      "Transparent Visiting Card",
-    ],
+    name: "Visiting Cards"
   },
+  {
+    img_url:"https://media.istockphoto.com/id/1008294438/vector/black-and-yellow-office-id-card-template.jpg?s=612x612&w=0&k=20&c=02n9LsLC2-cCkmVVrMs4woqNBDo19ZgrEdvZTetHPAc=",
+    name:"ID Cards"
+  },
+  {
+    img_url: "https://5.imimg.com/data5/AE/PN/MY-25908309/multicolor-lanyard-500x500.jpg",
+    name: "Lanyards",
+  },
+  {
+    img_url: "https://img.freepik.com/free-vector/watercolor-abstract-shapes-birthday-invitation_23-2149089279.jpg",
+    name: "Invitations",
+  },  
   {
     img_url: ".\\Product\\Flyers.jpg",
     name: "Flyers",
@@ -24,10 +22,6 @@ const cardlist = [
   {
     img_url: ".\\Product\\Broucher.png",
     name: "Brochure's",
-  },
-  {
-    img_url: "https://easylabeling.com/wp-content/uploads/Set-of-vintage-premium-quality-labels.jpg",
-    name: "Labels",
   },
   {
     img_url: "https://img.freepik.com/free-vector/beautiful-food-menu-design-template_23-2149061881.jpg",
@@ -38,16 +32,16 @@ const cardlist = [
     name: "Booklet's",
   },
   {
-    img_url: "https://img.freepik.com/free-vector/watercolor-abstract-shapes-birthday-invitation_23-2149089279.jpg",
-    name: "Invitations",
-  },  
+    img_url:"https://www.printplace.com/blog/wp-content/uploads/2017/05/Mock-Up.jpg",
+    name:"Catalogue"
+  },
+  {
+    img_url: "https://easylabeling.com/wp-content/uploads/Set-of-vintage-premium-quality-labels.jpg",
+    name: "Labels",
+  },
   {
     img_url: "https://graphicsfamily.com/wp-content/uploads/2020/06/Night-Club-event-ticket-template-1180x674.jpg",
     name: "Event Tickets",
-  },
-  {
-    img_url: "https://5.imimg.com/data5/AE/PN/MY-25908309/multicolor-lanyard-500x500.jpg",
-    name: "Lanyards",
   },
   {
     img_url: "https://printroo.com.au/wp-content/uploads/2013/07/envelope.png",
@@ -58,12 +52,12 @@ const cardlist = [
     name:"Photo Frames"
   },
   {
-    img_url:"https://niprint.co.uk/wp-content/uploads/2021/09/ncr_A5_3-600x600.jpg",
-    name:"Bill Books"
-  },
-  {
     img_url:"https://i0.wp.com/www.dotprint.co.uk/wp-content/uploads/2021/02/plan-printing-dotprint.jpg",
     name:"Plan Printing"
+  },
+  {
+    img_url:"https://niprint.co.uk/wp-content/uploads/2021/09/ncr_A5_3-600x600.jpg",
+    name:"Bill Books"
   },
   {
     img_url:"https://transform.octanecdn.com/cdn/https://octanecdn.com/colorvisionprintingcom/colorvisionprintingcom_545213800.png",
@@ -74,14 +68,13 @@ const cardlist = [
     name:"Wiro Binding"
   },
   {
-    img_url:"https://media.istockphoto.com/id/1008294438/vector/black-and-yellow-office-id-card-template.jpg?s=612x612&w=0&k=20&c=02n9LsLC2-cCkmVVrMs4woqNBDo19ZgrEdvZTetHPAc=",
-    name:"ID Cards"
+    img_url:"https://marketplace.canva.com/EAFGv9WhSmc/1/0/1600w/canva-blue-and-yellow-minimalist-employee-of-the-month-certificate-jaIc19nYjY4.jpg",
+    name:"Certificate's"
   },
-  {
-    img_url:"https://www.printplace.com/blog/wp-content/uploads/2017/05/Mock-Up.jpg",
-    name:"Catlogs"
-  },
-
+  // {
+  //   img_url:"https://www.stampmakers.in/wp-content/uploads/2023/10/name-plates.jpg",
+  //   name: "NameBoards"
+  // }
 ]
 
 export default cardlist;

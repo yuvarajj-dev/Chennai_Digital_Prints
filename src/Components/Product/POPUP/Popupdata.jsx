81b17ec6c9fdc5903edb92c8af086c67b9fda2f7
yuvarajj-dev <yuvarajj.developer@gmail.com>
2024-05-06 16:20:35 +0530

    push

--- src/Components/Product/POPUP/Popupdata.jsx
+++ src/Components/Product/POPUP/Popupdata.jsx
@@ -126,6 +126,166 @@ const Product_val =  [
     {
       Product_name: 'Visiting Cards',
       Product_path: '.\\Product\\Products\\visitingcard 4.jpg'
+    },
+    {
+      Product_name: "Bill Books",
+      Product_path: '.\\Product\\Products\\bill1.jpg'
+    },
+    {
+      Product_name: "Bill Books",
+      Product_path: '.\\Product\\Products\\bill3.jpg'
+    },
+    {
+      Product_name: "Bill Books",
+      Product_path: '.\\Product\\Products\\bill4.jpg'
+    },
+    {
+      Product_name: "Bill Books",
+      Product_path: '.\\Product\\Products\\billi2.jpg'
+    },
+    {
+      Product_name: "Brochure's",
+      Product_path: '.\\Product\\Products\\Broucher 1.jpg'
+    },
+    {
+      Product_name: "Brochure's",
+      Product_path: '.\\Product\\Products\\Broucher 2.jpg'
+    },
+    {
+      Product_name: "Brochure's",
+      Product_path: '.\\Product\\Products\\Broucher 3.jpg'
+    },
+    {
+      Product_name: "Brochure's",
+      Product_path: '.\\Product\\Products\\Broucher 4.jpg'
+    },
+    {
+      Product_name: "Catalogue",
+      Product_path: '.\\Product\\Products\\Catlog 1.jpg'
+    },
+    {
+      Product_name: "Catalogue",
+      Product_path: '.\\Product\\Products\\Catlog 2.jpg'
+    },
+    {
+      Product_name: "Catalogue",
+      Product_path: '.\\Product\\Products\\Catlog 3.jpg'
+    },
+    {
+      Product_name: "Catalogue",
+      Product_path: '.\\Product\\Products\\Catlog 4.jpg'
+    },
+    {
+      Product_name: "Certificate's",
+      Product_path: '.\\Product\\Products\\cer1.jpg'
+    },
+    {
+      Product_name: "Certificate's",
+      Product_path: '.\\Product\\Products\\cer2.jpg'
+    },
+    {
+      Product_name: "Certificate's",
+      Product_path: '.\\Product\\Products\\cer3.jpg'
+    },
+    {
+      Product_name: "Certificate's",
+      Product_path: '.\\Product\\Products\\cer4.jpg'
+    },
+    {
+      Product_name: "Envelopes",
+      Product_path: '.\\Product\\Products\\envelope.jpg'
+    },
+    {
+      Product_name: "Envelopes",
+      Product_path: '.\\Product\\Products\\enveloper 4.jpg'
+    },
+    {
+      Product_name: "Envelopes",
+      Product_path: '.\\Product\\Products\\enveloper2.jpg'
+    },
+    {
+      Product_name: "Envelopes",
+      Product_path: '.\\Product\\Products\\enveloper3.jpg'
+    },
+    {
+      Product_name: "ID Cards",
+      Product_path: '.\\Product\\Products\\IDCARD 1.jpg'
+    },
+    {
+      Product_name: "ID Cards",
+      Product_path: '.\\Product\\Products\\IDCARD 2.jpg'
+    },
+    {
+      Product_name: "ID Cards",
+      Product_path: '.\\Product\\Products\\IDCARD 3.jpg'
+    },
+    {
+      Product_name: "ID Cards",
+      Product_path: '.\\Product\\Products\\IDCARD 4.jpg'
+    },
+    {
+      Product_name: "Photo Frames",
+      Product_path: '.\\Product\\Products\\photo.jpg'
+    },
+    {
+      Product_name:"Photo Frames",
+      Product_path: '.\\Product\\Products\\photo2.jpg'
+    },
+    {
+      Product_name: "Photo Frames",
+      Product_path: '.\\Product\\Products\\photo3.jpg'
+    },
+    {
+      Product_name: "Photo Frames",
+      Product_path: '.\\Product\\Products\\photo4.jpg'
+    },
+    {
+      Product_name: "Plan Printing",
+      Product_path: '.\\Product\\Products\\plan.jpg'
+    },
+    {
+      Product_name: "Plan Printing",
+      Product_path: '.\\Product\\Products\\plan2.jpg'
+    },
+    {
+      Product_name: "Plan Printing",
+      Product_path: '.\\Product\\Products\\plan3.jpg'
+    },
+    {
+      Product_name: "Plan Printing",
+      Product_path: '.\\Product\\Products\\plan4.jpg'
+    },
+    {
+      Product_name: "Spiral Binding",
+      Product_path: '.\\Product\\Products\\spiral.jpg'
+    },
+    {
+      Product_name: "Spiral Binding",
+      Product_path: '.\\Product\\Products\\spiral2.jpg'
+    },
+    {
+      Product_name: "Spiral Binding",
+      Product_path: '.\\Product\\Products\\spiral3.jpg'
+    },
+    {
+      Product_name: "Spiral Binding",
+      Product_path: '.\\Product\\Products\\spiral4.jpg'
+    },
+    {
+      Product_name: "Wiro Binding",
+      Product_path: '.\\Product\\Products\\wiro.jpg'
+    },
+    {
+      Product_name: "Wiro Binding",
+      Product_path: '.\\Product\\Products\\wiro2.jpg'
+    },
+    {
+      Product_name: "Wiro Binding",
+      Product_path: '.\\Product\\Products\\wiro3.jpg.jpg'
+    },
+    {
+      Product_name: "Wiro Binding",
+      Product_path: '.\\Product\\Products\\wiro4.jpg'
     }
   ]
   export default Product_val;
